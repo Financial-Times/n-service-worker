c7c268e9ce069e7492c6bf8406b3a4eef78a6ce6
Patrick Hamann <patrick.hamann@ft.com>
2015-08-20 12:54:48 +0100

    Catch registration errors

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ module.exports  = {
 	init: function(flags) {
 		if ('serviceWorker' in navigator) {
 			if(flags.get('serviceWorkerExperiments')) {
-				navigator.serviceWorker.register('/__sw.js');
+				navigator.serviceWorker
+					.register('/__sw.js')
+					.catch(function(err) {
+						throw err;
+					})
 			}
 		}
 	}
