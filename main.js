001450fe49ee98f947d02db261d530679fe319e0
Rhys Evans <wheresrhys@gmail.com>
2016-07-15 16:26:41 +0100

    pass flags in to sw to turn features on and off
    * pass flags in to sw to turn features on and off
    
    * reinstated returning of promises
    
    * flagging actually works

--- main.js
+++ main.js
@@ -1,14 +1,43 @@
+const message = msg => {
+
+	return new Promise((resolve, reject) => {
+		// Create a Message Channel
+		const messageChannel = new MessageChannel();
+
+		// Handler for recieving message reply from service worker
+		messageChannel.port1.onmessage = ev => {
+			if (ev.data.error) {
+				reject(ev.data.error);
+			} else {
+				resolve(ev.data);
+			}
+		};
+
+		// Send message to service worker along with port for reply
+		navigator.serviceWorker.controller.postMessage(msg, [messageChannel.port2]);
+	});
+}
+
 const register = flags => {
-	('serviceWorker' in navigator && flags.get('serviceWorker')) ?
-		navigator.serviceWorker.register('/__sw.js') :
-		Promise.resolve()
+	if ('serviceWorker' in navigator && flags.get('serviceWorker')) {
+		return navigator.serviceWorker.register('/__sw.js')
+			.then(() => {
+				return message({
+					type: 'flagsUpdate',
+					flags: JSON.parse(JSON.stringify(flags)) // to avoid error caused by the getters
+				});
+			});
+	};
+
+	return Promise.resolve(false);
 };
 
 const unregister = () => {
-	'serviceWorker' in navigator ?
-		navigator.serviceWorker.getRegistration()
-			.then(registration => registration ? registration.unregister() : Promise.resolve(false)) :
-		Promise.resolve(false)
+	if ('serviceWorker' in navigator) {
+		return navigator.serviceWorker.getRegistration()
+			.then(registration => registration ? registration.unregister() : false);
+	}
+	return Promise.resolve(false);
 };
 
-export { register, unregister };
+export { register, unregister, message };
--- src/__sw.js
+++ src/__sw.js
@@ -2,7 +2,27 @@ import toolbox from 'sw-toolbox';
 
 const fonts = ['MetricWeb-Regular', 'MetricWeb-Semibold', 'FinancierDisplayWeb-Regular'];
 const fontsVersion = '1.3.0';
+// note because flags don't exist on first page view
+// use flags typically to RETRIEVE from cache
+// if flag === false can clear cache
+// if flag === undefined can put in cache but not retrieve
+// if flag === true can put and retrieve from cache
+let flags = {}; //eslint-disable-line
 
+self.addEventListener('message', msg => {
+	const data = msg.data;
+	switch (data.type) {
+		case 'flagsUpdate' :
+			flags = Object.freeze(data.flags);
+			break;
+	}
+});
+
+self.addEventListener('activate', () => {
+	// do some stuff to enable/disable caches and other features based on value of current flags
+});
+
+// TODO need to version the fonts cache
 // cache fronts upfront
 toolbox.precache(
 	fonts.map(font => `https://next-geebee.ft.com/build/v2/files/o-fonts-assets@${fontsVersion}/${font}.woff?`)
