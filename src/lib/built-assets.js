426269b22f62566f839548e57f474f36fcc845cf
Rhys Evans <wheresrhys@gmail.com>
2016-07-18 15:23:45 +0100

    added more caching rules for more resources (#10)

---
+++ src/lib/built-assets.js
@@ -0,0 +1,30 @@
+import toolbox from 'sw-toolbox';
+import flags from './flags';
+
+
+// TODO have one cache for our more actively developed apps,
+// another with longer cache life for errors, opt-out etc
+const cacheOptions = {
+	origin: 'https://next-geebee.ft.com',
+	cache: {
+		name: 'built-assets',
+		maxEntries: 20,
+		// our code base moves so fast, pointless caching enything longer than a few days
+		maxAgeSeconds: 60 * 60 * 24 * 5
+	}
+};
+
+
+self.addEventListener('message', msg => {
+	const data = msg.data;
+	if (data.type === 'hashedAssetsUpdate' && flags.get('swPrecacheHashedAssets')) {
+		// TODO work out how an app can retrieve info about the asset hashes for the other 3 main apps
+	}
+});
+
+// prod
+toolbox.router.get('/hashed-assets/:appName/:assetHash/:cssName.css', toolbox.cacheFirst, cacheOptions);
+toolbox.router.get('/hashed-assets/:appName/:assetHash/:cssName.js', toolbox.cacheFirst, cacheOptions);
+// local
+toolbox.router.get('/:appName/:cssName.css', toolbox.cacheFirst, cacheOptions);
+toolbox.router.get('/:appName/:cssName.js', toolbox.cacheFirst, cacheOptions);
---
+++ src/lib/fonts.js
@@ -0,0 +1,19 @@
+import toolbox from 'sw-toolbox';
+
+const fonts = ['MetricWeb-Regular', 'MetricWeb-Semibold', 'FinancierDisplayWeb-Regular'];
+const fontsVersion = '1.3.0';
+const cacheOptions = {
+	origin: 'https://next-geebee.ft.com',
+	cache: {
+		name: `fonts:${fontsVersion}`
+	}
+};
+
+// TODO need to version the fonts cache
+// cache fronts upfront
+toolbox.precache(
+	fonts.map(font => `https://next-geebee.ft.com/build/v2/files/o-fonts-assets@${fontsVersion}/${font}.woff?`)
+);
+
+// fonts route
+toolbox.router.get('/build/v2/files/o-fonts-assets@:version/:font.woff', toolbox.cacheFirst, cacheOptions);
