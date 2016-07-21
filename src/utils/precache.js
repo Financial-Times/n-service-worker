dada78122b01d527b7f04ef69c4dcaaf21613a98
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-07-21 15:17:23 +0100

    Linting

--- src/utils/precache.js
+++ src/utils/precache.js
@@ -3,8 +3,8 @@ import toolbox from 'sw-toolbox';
 // NOTE: not using sw toolbox's precache, as can't set the cache name
 export default (urls, options) => {
 	self.addEventListener('install', ev => {
-	  	ev.waitUntil(
-	  		Promise.all(urls.map(url => toolbox.cache(url, options)))
-	  	);
+		ev.waitUntil(
+			Promise.all(urls.map(url => toolbox.cache(url, options)))
+		);
 	});
 }
