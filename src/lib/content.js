c6b24cefb1dca2c7ab3041ec52cca1fc07431865
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-07-21 12:11:06 +0100

    Ability to send message to cache content

--- src/lib/content.js
+++ src/lib/content.js
@@ -1,11 +1,21 @@
 import toolbox from 'sw-toolbox';
 
 const cacheOptions = {
-	origin: 'https://next-geebee.ft.com',
+	origin: 'https://*.ft.com',
 	cache: {
 		name: 'next:content',
 		// our code base moves so fast, pointless caching enything longer than a few days
 		maxAgeSeconds: 60
 	}
 };
+
 toolbox.router.get('/', toolbox.fastest, cacheOptions);
+
+self.addEventListener('message', ev => {
+	const msg = ev.data;
+	if (msg.type === 'cacheContent') {
+		(msg.content || []).forEach(url => {
+			toolbox.cache(url, cacheOptions);
+		});
+	}
+});
