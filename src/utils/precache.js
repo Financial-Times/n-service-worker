39e92912ca993cf8f2973a4f0982b8eecedc4e9f
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-07-21 15:12:12 +0100

    Roll our own precache

---
+++ src/utils/precache.js
@@ -0,0 +1,10 @@
+import toolbox from 'sw-toolbox';
+
+// NOTE: not using sw toolbox's precache, as can't set the cache name
+export default (urls, options) => {
+	self.addEventListener('install', ev => {
+	  	ev.waitUntil(
+	  		Promise.all(urls.map(url => toolbox.cache(url, options)))
+	  	);
+	});
+}
