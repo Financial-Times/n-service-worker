d3fd95a6b6bba898cd752c47f71aee8a700818d5
Rhys Evans <wheresrhys@gmail.com>
2016-07-26 12:32:43 +0100

    fix uglification

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = nWebpack({
 	entry: {
 		'./dist/__sw.js': './src/__sw.js'
 	},
-	include: [path.resolve('./src')],
+	include: [path.resolve('./src'), path.resolve('./node_modules/indexeddb')],
 	loaders: [
 		{
 			test: /indexeddb-promised\.js$/,
