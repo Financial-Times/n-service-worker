3cff659950a0373d562c577beb7af5b878749b3a
Rhys Evans <rhys.evans@ft.com>
2016-07-26 10:03:42 +0100

    temporarily disable content task

--- src/__sw.js
+++ src/__sw.js
@@ -7,7 +7,6 @@ import './lib/image';
 import './lib/built-assets';
 import './lib/n-ui';
 import './lib/polyfill';
-import './lib/content';
+// import './lib/content';
 import './lib/comments';
 import './lib/myft';
-
