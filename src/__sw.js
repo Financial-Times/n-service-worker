7e51ff5ffda1085fbff440afc5146e1b04ebfcae
Rhys Evans <rhys.evans@ft.com>
2016-07-27 12:10:32 +0100

    moving stuff around and rough import of myt push

--- src/__sw.js
+++ src/__sw.js
@@ -4,17 +4,20 @@ toolbox.options.cache.name = 'next';
 import './utils/flags';
 
 // generic assets
-import './lib/fonts';
-import './lib/image';
-import './lib/built-assets';
-import './lib/n-ui';
-import './lib/polyfill';
-import './lib/comments';
+import './caches/fonts';
+import './caches/image';
+import './caches/built-assets';
+import './caches/n-ui';
+import './caches/polyfill';
+import './caches/comments';
 
 // user-specific things
-// import './lib/content';
-import './lib/session';
-import './lib/myft';
-import './lib/ads';
+// import './offline/content';
+import './caches/session';
+import './caches/myft';
+import './caches/ads';
+
+
+// import './push/myft';
 
 toolbox.options.cache.name = 'next';
