cc8203705c543c41677b83e16941fd937331eb24
Rhys Evans <wheresrhys@gmail.com>
2016-09-12 15:46:47 +0100

    *fix* fonts in firefox

--- test/caches/fonts.spec.js
+++ test/caches/fonts.spec.js
@@ -1,7 +1,16 @@
 /* global SWTestHelper */
-describe('fonts', () => {
 
-	['MetricWeb-Regular', 'MetricWeb-Semibold', 'FinancierDisplayWeb-Regular'].map(font => {
+const useragent = require('useragent');
+
+describe('fonts', () => {
+	const fonts = ['MetricWeb-Regular', 'MetricWeb-Semibold']
+
+	// FIXME: Fonts in firefox are being weird generally, this one is particularly bad and fails every time
+	if (useragent.is(navigator.userAgent).chrome) {
+		fonts.push('FinancierDisplayWeb-Regular')
+	}
+
+	fonts.map(font => {
 		const url = `https://next-geebee.ft.com/build/v2/files/o-fonts-assets@1.3.0/${font}.woff?`;
 		const expiry = 'no-expiry';
 		const cacheName ='fonts';
