068f866d69501eeac2e661d306a5cd86aa3a6386
Rhys Evans <wheresrhys@gmail.com>
2016-11-17 16:10:50 +0000

    tests for cache util (#52)
    * midway through tests for cache util
    
    * started to implement lazier cache invalidation
    
    * finished cache tests
    
    * fixed interference between cache and sw integration tests
    
    * lint
    
    * more tweaking
    
    * disable deploying for now

--- Makefile
+++ Makefile
@@ -10,10 +10,13 @@ server:
 run: build-dev server
 
 deploy: build-production
-	nht deploy-static `find . -path "./dist/*"` --strip 1 --bucket ft-next-service-worker-prod --cache-control "max-age=0" --surrogate-control "max-age=600; stale-while-revalidate=60; stale-on-error=3600" --monitor
+	# nht deploy-static `find . -path "./dist/*"` --strip 1 --bucket ft-next-service-worker-prod --cache-control "max-age=0" --surrogate-control "max-age=600; stale-while-revalidate=60; stale-on-error=3600" --monitor
 
 test-unit:
 	karma start
 
-test-dev:
-	karma start --autoWatch=true --singleRun=false
+test-chrome:
+	karma start --autoWatch=true --singleRun=false --browsers=Chrome
+
+test-firefox:
+	karma start --autoWatch=true --singleRun=false --browsers=Firefox
--- src/push/myft.js
+++ src/push/myft.js
@@ -1,138 +1,138 @@
-/* global clients:false*/
-import eagerFetch from '../utils/eager-fetch';
-import track from '../utils/track';
-import {getFlag} from '../utils/flags';
+// /* global clients:false*/
+// import eagerFetch from '../utils/eager-fetch';
+// import track from '../utils/track';
+// import {getFlag} from '../utils/flags';
 
-const title = 'New article in your myFT page';
-const icon = 'https://next-geebee.ft.com/assets/icons/myft-logo-pink-bg.png';
+// const title = 'New article in your myFT page';
+// const icon = 'https://next-geebee.ft.com/assets/icons/myft-logo-pink-bg.png';
 
-let lastSentIds = [];
+// let lastSentIds = [];
 
-setInterval(() => {
-	lastSentIds = [];
-}, 1000 * 60 * 10);
+// setInterval(() => {
+// 	lastSentIds = [];
+// }, 1000 * 60 * 10);
 
-self.addEventListener('push', ev => {
-	if (!getFlag('swMyftPush')) {
-		return;
-	}
+// self.addEventListener('push', ev => {
+// 	if (!getFlag('swMyftPush')) {
+// 		return;
+// 	}
 
-	let tag = 'next-myft-article';
-	let notificationData = {};
-	let body;
+// 	let tag = 'next-myft-article';
+// 	let notificationData = {};
+// 	let body;
 
-	function showDefaultNotification (err) {
+// 	function showDefaultNotification (err) {
 
-		console.error(err);
+// 		console.error(err);
 
-		track({
-			category: 'push',
-			action: 'shown',
-			context: {
-				silent: false,
-				type: 'default'
-			},
-			content: { uuid: notificationData.id }
-		});
+// 		track({
+// 			category: 'push',
+// 			action: 'shown',
+// 			context: {
+// 				silent: false,
+// 				type: 'default'
+// 			},
+// 			content: { uuid: notificationData.id }
+// 		});
 
-		return self.registration.showNotification(title, {
-			requireInteraction: false,
-			icon: icon,
-			tag: tag,
-			data: {
-				id: ''
-			}
-		});
-	};
+// 		return self.registration.showNotification(title, {
+// 			requireInteraction: false,
+// 			icon: icon,
+// 			tag: tag,
+// 			data: {
+// 				id: ''
+// 			}
+// 		});
+// 	};
 
-	ev.waitUntil(
-		eagerFetch('/myft/following.json?since=-1h', {
-			headers: {
-				'Content-Type': 'application/json',
-				'Accept': 'application/json'
-			},
-			credentials: 'include'
-		})
-		.then(res => res.json())
-		.then(data => {
+// 	ev.waitUntil(
+// 		eagerFetch('/myft/following.json?since=-1h', {
+// 			headers: {
+// 				'Content-Type': 'application/json',
+// 				'Accept': 'application/json'
+// 			},
+// 			credentials: 'include'
+// 		})
+// 		.then(res => res.json())
+// 		.then(data => {
 
-			if (data && data.length) {
-				let index = 0;
+// 			if (data && data.length) {
+// 				let index = 0;
 
-				while (data[index] && data[index].id && lastSentIds.indexOf(data[index].id) >= 0) {
-					index++;
-				}
-				if (data[index] && data[index].id) {
-					lastSentIds.push(data[index].id);
-					title = data[index].headline;
-					body = data[index].subheading;
-					tag = data[index].id;
-					notificationData = { id: data[index].id };
-				}
-			}
+// 				while (data[index] && data[index].id && lastSentIds.indexOf(data[index].id) >= 0) {
+// 					index++;
+// 				}
+// 				if (data[index] && data[index].id) {
+// 					lastSentIds.push(data[index].id);
+// 					title = data[index].headline;
+// 					body = data[index].subheading;
+// 					tag = data[index].id;
+// 					notificationData = { id: data[index].id };
+// 				}
+// 			}
 
-			track({
-				category: 'push',
-				action: 'shown',
-				content: { uuid: notificationData.id }
-			});
+// 			track({
+// 				category: 'push',
+// 				action: 'shown',
+// 				content: { uuid: notificationData.id }
+// 			});
 
-			return self.registration.showNotification(title, {
-				requireInteraction: false,
-				body: body,
-				tag: tag,
-				icon: icon,
-				data: notificationData
-			});
+// 			return self.registration.showNotification(title, {
+// 				requireInteraction: false,
+// 				body: body,
+// 				tag: tag,
+// 				icon: icon,
+// 				data: notificationData
+// 			});
 
-		})
-		.catch(showDefaultNotification)
-	);
+// 		})
+// 		.catch(showDefaultNotification)
+// 	);
 
-});
+// });
 
-self.addEventListener('notificationclick', ev => {
-	if (!getFlag('swMyftPush')) {
-		return;
-	}
-	// Android doesn't close the notification when you click on it
-	// See: http://crbug.com/463146
-	ev.notification.close();
+// self.addEventListener('notificationclick', ev => {
+// 	if (!getFlag('swMyftPush')) {
+// 		return;
+// 	}
+// 	// Android doesn't close the notification when you click on it
+// 	// See: http://crbug.com/463146
+// 	ev.notification.close();
 
-	// This looks to see if the current window is already open and
-	// focuses if it is
-	ev.waitUntil(
-		clients.matchAll({
-			type: 'window'
-		})
-		.then(clientList => {
-			let url;
-			if (Notification.prototype.hasOwnProperty('data') && ev.notification.data.id) {
-				url = '/content/' + ev.notification.data.id;
-			} else {
-				url = '/myft/following';
-			}
+// 	// This looks to see if the current window is already open and
+// 	// focuses if it is
+// 	ev.waitUntil(
+// 		clients.matchAll({
+// 			type: 'window'
+// 		})
+// 		.then(clientList => {
+// 			let url;
+// 			if (Notification.prototype.hasOwnProperty('data') && ev.notification.data.id) {
+// 				url = '/content/' + ev.notification.data.id;
+// 			} else {
+// 				url = '/myft/following';
+// 			}
 
-			url = url + '#myft:notification:push';
+// 			url = url + '#myft:notification:push';
 
-			track({
-				category: 'push',
-				action: 'click',
-				context: {
-					url: 'https://next.ft.com' + url
-				},
-				content: { uuid: ev.notification.data ? ev.notification.data.id : 'default' }
-			});
+// 			track({
+// 				category: 'push',
+// 				action: 'click',
+// 				context: {
+// 					url: 'https://next.ft.com' + url
+// 				},
+// 				content: { uuid: ev.notification.data ? ev.notification.data.id : 'default' }
+// 			});
 
-			for (let i = 0; i < clientList.length; i++) {
-				let client = clientList[i];
-				if (client.url.indexOf(url) > 0 && 'focus' in client) {
-					return client.focus();
-				}
-			}
-			if (clients.openWindow) {
-				return clients.openWindow(url);
-			}
-		})
-	);
-});
+// 			for (let i = 0; i < clientList.length; i++) {
+// 				let client = clientList[i];
+// 				if (client.url.indexOf(url) > 0 && 'focus' in client) {
+// 					return client.focus();
+// 				}
+// 			}
+// 			if (clients.openWindow) {
+// 				return clients.openWindow(url);
+// 			}
+// 		})
+// 	);
+// });
