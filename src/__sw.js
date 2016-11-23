2f0f9978b4c8946e20a8f03b4a6b616cc87e5ec2
Adam Tavener <a.tavvy@gmail.com>
2016-11-23 12:02:14 +0000

    disable other features for MVP release

--- src/__sw.js
+++ src/__sw.js
@@ -3,22 +3,24 @@
 import './utils/flags';
 
 // generic assets
-// import './caches/fonts';
-// import './caches/image';
-// import './caches/built-assets';
-// import './caches/n-ui';
-// import './caches/polyfill';
-// import './caches/comments';
+// import './caches/fonts'; *
+// import './caches/image'; *
+// import './caches/built-assets'; *
+// import './caches/n-ui'; *
+// import './caches/polyfill'; *
+// import './caches/comments'; *
 
 // user-specific things
 // import './offline/content';
 // import './caches/session';
 // import './caches/myft';
-// import './caches/ads';
+// import './caches/ads'; *
 // import './push/myft';
 
 // NOTE: this is mounted last as it
 // contains a catch all route handler
+// imports labelled with * have been disabled
+// while we do MVP testing
 import './offline/404-test';
 
 import router from './utils/router';
--- test/setup.js
+++ test/setup.js
@@ -4,7 +4,7 @@ import { passFlags } from '../main'
 before(() => SWTestHelper.resetEnv());
 before(() => SWTestHelper.installSW('/integration-sw.js')
 	.then(() => (SWTestHelper.installedAt = Date.now()))
-	.then(() => console.log('sw installed'))
+	// .then(() => console.log('sw installed'))
 )
 afterEach(() => passFlags({}));
 after(() => SWTestHelper.resetEnv());
