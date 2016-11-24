df66b92af15c4a13502db6db0e876c2b99b176a1
phamann <patrickhamann@gmail.com>
2016-11-24 15:04:47 +0000

    Update naviagation static image assets in image cache

--- src/caches/image.js
+++ src/caches/image.js
@@ -4,29 +4,31 @@ import { getHandler } from '../utils/handlers';
 import precache from '../utils/precache';
 
 const options = {
-	origin: 'https://next-geebee.ft.com',
+	origin: 'https://www.ft.com',
 	cache: {
 		name: 'image'
 	}
 };
 
 const headerImages = [
-	'fticon:hamburger?source=o-icons&tint=%23505050,%23505050&format=svg',
+	'fticon-v1:hamburger?source=o-icons&tint=%23505050,%23505050&format=svg',
 	'ftlogo:brand-ft-masthead?source=o-header&tint=%23505050,%23505050&format=svg',
-	'ftlogo:brand-myft?source=o-header&tint=%23505050,%23505050&format=svg'
+	'ftlogo:brand-myft?source=o-header&tint=%23505050,%23505050&format=svg',
+	'fticon-v1:search?source=o-icons&tint=%23505050,%23505050&format=svg'
 ]
 
 precache(
 	options.cache.name,
-	headerImages.map(image => `https://next-geebee.ft.com/image/v1/images/raw/${image}`),
+	headerImages.map(image => new Request(`https://www.ft.com/__origami/service/image/v2/images/raw/${image}`, {
+		mode: 'cors'
+	})),
 	{ maxAge: -1 }
 );
 
 const cacheFirst = getHandler({strategy: 'cacheFirst', upgradeToCors: true})
 
 //TODO - somethong for content images
-
-router.get('/image/v1/images/raw/fticon*', cacheFirst, options);
-router.get('/image/v1/images/raw/ftlogo*', cacheFirst, options);
-router.get('/image/v1/images/raw/ftsocial*', cacheFirst, options);
+router.get('/__origami/service/image/v2/images/raw/fticon*', cacheFirst, options);
+router.get('/__origami/service/image/v2/images/raw/ftlogo*', cacheFirst, options);
+router.get('/image/v2/images/raw/ftsocial*', cacheFirst, options);
 router.get('/assets/*', cacheFirst, options);
