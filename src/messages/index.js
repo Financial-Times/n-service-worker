bdcf2dc67e827ad42f765af64a142f08b03c9b74
Adam Tavener <a.tavvy@gmail.com>
2016-12-12 14:17:44 +0000

    more cleanup

--- src/messages/index.js
+++ src/messages/index.js
@@ -6,7 +6,6 @@ const replyTo = (client, data) => client.postMessage(data);
 const replyToPage = (event) => replyTo.bind(null, event.ports[0]);
 
 export function messageHandler (ev) {
-	console.log('handler called with', ev)
 	const data = ev.data && ev.data.data ? ev.data.data : {};
 	const res = replyToPage(ev);
 
