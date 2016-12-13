1ee2ab1f695e7358cde5299798509344625c19fd
Adam Tavener <a.tavvy@gmail.com>
2016-12-13 11:13:01 +0000

    re-introduce absolute urls

--- config/precache.js
+++ config/precache.js
@@ -5,15 +5,15 @@ module.exports = {
 		'MetricWeb-Bold',
 		'FinancierDisplayWeb-Regular'
 	]
-		.map(font => `/__origami/service/build/v2/files/o-fonts-assets@1.3.0/${font}.woff?`),
+		.map(font => `https://www.ft.com/__origami/service/build/v2/files/o-fonts-assets@1.3.0/${font}.woff?`),
 	image: [
-		'/__origami/service/image/v2/images/raw/fticon-v1:hamburger?source=o-icons&tint=%23505050,%23505050&format=svg',
-		'/__origami/service/image/v2/images/raw/fticon-v1:cross?source=o-icons&tint=%23505050,%23505050&format=svg',
-		'/__origami/service/image/v2/images/raw/ftlogo:brand-ft-masthead?source=o-header&tint=%23505050,%23505050&format=svg',
-		'/__origami/service/image/v2/images/raw/ftlogo:brand-myft?source=o-header&tint=%23505050,%23505050&format=svg',
-		'/__origami/service/image/v2/images/raw/fticon-v1:search?source=o-icons&tint=%23505050,%23505050&format=svg',
-		'/__origami/service/image/v2/images/raw/fticon-v1:search?source=o-icons&tint=%23505050,%23505050&format=svg',
-		'/__origami/service/image/v2/images/raw/ftlogo:brand-nikkei-tagline?source=o-footer&format=svg',
-		'/__origami/service/image/v2/images/raw/ftlogo:brand-ft?source=next&tint=999999,999999'
+		'https://www.ft.com/__origami/service/image/v2/images/raw/fticon-v1:hamburger?source=o-icons&tint=%23505050,%23505050&format=svg',
+		'https://www.ft.com/__origami/service/image/v2/images/raw/fticon-v1:cross?source=o-icons&tint=%23505050,%23505050&format=svg',
+		'https://www.ft.com/__origami/service/image/v2/images/raw/ftlogo:brand-ft-masthead?source=o-header&tint=%23505050,%23505050&format=svg',
+		'https://www.ft.com/__origami/service/image/v2/images/raw/ftlogo:brand-myft?source=o-header&tint=%23505050,%23505050&format=svg',
+		'https://www.ft.com/__origami/service/image/v2/images/raw/fticon-v1:search?source=o-icons&tint=%23505050,%23505050&format=svg',
+		'https://www.ft.com/__origami/service/image/v2/images/raw/fticon-v1:search?source=o-icons&tint=%23505050,%23505050&format=svg',
+		'https://www.ft.com/__origami/service/image/v2/images/raw/ftlogo:brand-nikkei-tagline?source=o-footer&format=svg',
+		'https://www.ft.com/__origami/service/image/v2/images/raw/ftlogo:brand-ft?source=next&tint=999999,999999'
 	]
 }
