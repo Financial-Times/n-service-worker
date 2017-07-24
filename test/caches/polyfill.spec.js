ff129489864007287c3a749b531e1285eb7792ac
Rhys Evans <rhys.evans@ft.com>
2017-07-24 15:59:20 +0100

    fix polyfill service url  🐿2.5.10

--- test/caches/polyfill.spec.js
+++ test/caches/polyfill.spec.js
@@ -2,7 +2,7 @@
 describe('polyfill', () => {
 	SWTestBundles.checkCacheIsUsed({
 		assetLabel: 'polyfill service',
-		url: 'https://next-geebee.ft.com/polyfill/v2/polyfill.min.js?features=default,requestAnimationFrame,Promise,matchMedia,HTMLPictureElement,fetch,Array.prototype.find,Array.prototype.findIndex,IntersectionObserver&flags=gated&unknown=polyfill&callback=ftNextPolyfillServiceCallback&excludes=Symbol,Symbol.iterator,Symbol.species,Map,Set',
+		url: '/__origami/service/polyfill/v2/polyfill.min.js?features=default,requestAnimationFrame,Promise,matchMedia,HTMLPictureElement,fetch,Array.prototype.find,Array.prototype.findIndex,IntersectionObserver&flags=gated&unknown=polyfill&callback=ftNextPolyfillServiceCallback&excludes=Symbol,Symbol.iterator,Symbol.species,Map,Set',
 		expiry: 'no-expiry',
 		upgradeToCors: true,
 		cacheName: 'polyfill',
