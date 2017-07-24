3759c6501793e99c2b59fd31b264c3316f65d36b
Rhys Evans <rhys.evans@ft.com>
2017-07-24 12:37:42 +0100

    updated image tints  🐿2.5.10

--- config/precache.js
+++ config/precache.js
@@ -5,10 +5,10 @@ const appcache = {
 		'/__origami/service/build/v2/files/o-fonts-assets@1.3.0/FinancierDisplayWeb-Regular.woff?'
 	],
 	image: [
-		'/__origami/service/image/v2/images/raw/fticon-v1:hamburger?source=o-icons&tint=%23505050,%23505050&format=svg',
-		'/__origami/service/image/v2/images/raw/fticon-v1:search?source=o-icons&tint=%23505050,%23505050&format=svg',
-		'/__origami/service/image/v2/images/raw/ftlogo:brand-ft-masthead?source=o-header&tint=%23505050,%23505050&format=svg',
-		'/__origami/service/image/v2/images/raw/ftlogo:brand-myft?source=o-header&tint=%23505050,%23505050&format=svg'
+		'/__origami/service/image/v2/images/raw/ftlogo:brand-ft-masthead?source=o-header&tint=%2333302E,%2333302E&format=svg',
+		'/__origami/service/image/v2/images/raw/fticon-v1:hamburger?source=o-icons&tint=%2333302E,%2333302E&format=svg',
+		'/__origami/service/image/v2/images/raw/fticon-v1:search?source=o-icons&tint=%2333302E,%2333302E&format=svg',
+		'/__origami/service/image/v2/images/raw/ftlogo:brand-myft?source=o-header&tint=%2333302E,%2333302E&format=svg'
 	]
 };
 
@@ -18,7 +18,7 @@ const sw = {
 	]),
 	image: appcache.image.concat([
 		// needed for the offline app - disabled for now
-		// '/__origami/service/image/v2/images/raw/fticon-v1:cross?source=o-icons&tint=%23505050,%23505050&format=svg',
+		// '/__origami/service/image/v2/images/raw/fticon-v1:cross?source=o-icons&tint=%2333302E,%2333302E&format=svg',
 		// '/__origami/service/image/v2/images/raw/ftlogo:brand-nikkei-tagline?source=o-footer&format=svg',
 		// '/__origami/service/image/v2/images/raw/ftlogo:brand-ft?source=next&tint=999999,999999',
 		// '/__origami/service/image/v2/images/raw/fticon-v1:refresh?source=o-icons&tint=%23FFFFFF,%23FFFFFF&format=svg',
