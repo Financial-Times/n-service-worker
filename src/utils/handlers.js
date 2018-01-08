340e0b36756c3613429c3e9d39e4e17b13333a13
Rhys Evans <rhys.evans@ft.com>
2018-01-08 19:14:32 +0000

    test for fastest strategy passes  🐿 v2.5.16

--- src/utils/handlers.js
+++ src/utils/handlers.js
@@ -23,7 +23,6 @@ const handlers = {
 	},
 
 	fastest: (request, values, options = {}) => {
-		console.log('as;kdjlkashdksajdsahdkjsahjk')
 		const cacheOptions = options.cache || {};
 		const openCache = cache(cacheOptions.name);
 
@@ -31,26 +30,16 @@ const handlers = {
 		// kickoff retrieving response from network and cache
 		const fromNetwork = fetch(request)
 			.then(res => {
-				console.log('from network', Date.now() - start)
 				return res;
 			});
 
 		const fromCache = openCache
-			.then(cache => {
-				console.log('open', Date.now() - start)
-				cache.get(request, cacheOptions)
-			})
+			.then(cache => cache.get(request, cacheOptions))
 			.then(res => {
-				console.log('get', Date.now() - start)
 				if (!res) {
 					throw 'request not found in cache';
 				}
-				console.log('from cache')
 				return res;
-			})
-			.catch(err => {
-				console.log(err);
-				throw err
 			});
 
 		// update the cache when the network response returns
@@ -60,11 +49,7 @@ const handlers = {
 			openCache
 		])
 			.then(([response, cache]) => {
-				console.log(response)
 				return cache.set(request, Object.assign({ response: response.clone() }, cacheOptions))
-					.then((it) => {
-						return it
-					})
 			});
 
 		// return a race between the two strategies
