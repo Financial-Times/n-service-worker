fff96ffc5199f0fdbf29776df3a47ddc17fcb567
Samuel Parkinson <51677+sjparkinson@users.noreply.github.com>
2018-07-24 12:34:50 +0100

    Remove support for the application cache. (#151)
    🐿 v2.10.0

--- Makefile
+++ Makefile
@@ -19,12 +19,6 @@ build:
 build-production:
 	webpack --bail -p
 
-build-appcache:
-	node appcache/generate.js
-	node appcache/generate.js landing
-	cp appcache/loader.html dist/__appcache-manifest-loader.html
-	cp appcache/loader-landing.html dist/__appcache-manifest-loader-landing.html
-
 # TODO: Add proper integration tests with nightwatch
 integration-test:
 	karma start
@@ -50,6 +44,6 @@ run: build-dev server
 server:
 	http-server dist -p $(PORT) -c-1
 
-deploy: build-production build-appcache
+deploy: build-production
 	nht deploy-static `find . -path "./dist/*"` --strip 1 --bucket ft-next-service-worker-prod \
 		--cache-control "max-age=0" --surrogate-control "max-age=600; stale-while-revalidate=60; stale-on-error=3600" --monitor
--- config/precache.js
+++ config/precache.js
@@ -1,8 +1,9 @@
-const appcache = {
+const sw = {
 	'fonts-v1': [
 		'/__origami/service/build/v2/files/o-fonts-assets@1.3.2/MetricWeb-Regular.woff',
 		'/__origami/service/build/v2/files/o-fonts-assets@1.3.2/MetricWeb-Semibold.woff',
-		'/__origami/service/build/v2/files/o-fonts-assets@1.3.2/FinancierDisplayWeb-Regular.woff'
+		'/__origami/service/build/v2/files/o-fonts-assets@1.3.2/FinancierDisplayWeb-Regular.woff',
+		'/__origami/service/build/v2/files/o-fonts-assets@1.3.2/FinancierDisplayWeb-Bold.woff'
 	],
 	'image-v1': [
 		'/__origami/service/image/v2/images/raw/ftlogo:brand-ft-masthead?source=o-header&tint=%2333302E,%2333302E&format=svg',
@@ -12,19 +13,4 @@ const appcache = {
 	]
 };
 
-const sw = {
-	'fonts-v1': appcache['fonts-v1'].concat([
-		'/__origami/service/build/v2/files/o-fonts-assets@1.3.2/FinancierDisplayWeb-Bold.woff'
-	]),
-	'image-v1': appcache['image-v1'].concat([
-		// needed for the offline app - disabled for now
-		// '/__origami/service/image/v2/images/raw/fticon-v1:cross?source=o-icons&tint=%2333302E,%2333302E&format=svg',
-		// '/__origami/service/image/v2/images/raw/ftlogo:brand-nikkei-tagline?source=o-footer&format=svg',
-		// '/__origami/service/image/v2/images/raw/ftlogo:brand-ft?source=next&tint=999999,999999',
-		// '/__origami/service/image/v2/images/raw/fticon-v1:refresh?source=o-icons&tint=%23FFFFFF,%23FFFFFF&format=svg',
-		// '/__origami/service/image/v2/images/raw/fticon-v1:arrow-down?source=o-icons&tint=%2326747A,%2326747A&format=svg',
-		// '/__origami/service/image/v2/images/raw/fticon-v1:cross?source=o-icons&tint=%23FFFFFF,%23FFFFFF&format=svg',
-	])
-};
-
-module.exports = { appcache, sw };
+module.exports = { sw };
