7b76c445e203225f0583cdfbe842c3ab465d438c
Luke Kavanagh <kavanagh@users.noreply.github.com>
2024-03-18 11:30:39 +0000

    Unregister self
    This removes all the service worker code and unregisters itself.
    
    We're doing this because despite trying to unregister the service worker form applications, it's still alive for some browsers (a couple of hundred users).
    
    The internet recommends trying to get the SW to unregister itself and ensure the CDN cache for /__sw.js is purged.

--- src/messages/controllers/client-id-lookup.js
+++
@@ -1,4 +0,0 @@
-
-export default function (data, source, event, res) {
-	res({ clientId: event.source.id });
-}
--- test/integration/setup.js
+++
@@ -1,9 +0,0 @@
-/* global SWTestHelper */
-import { passFlags } from '../../main';
-
-before(() => SWTestHelper.resetEnv());
-before(() => SWTestHelper.installSW('/integration-sw.js')
-	.then(() => (SWTestHelper.installedAt = Date.now()))
-);
-afterEach(() => passFlags({}));
-after(() => SWTestHelper.resetEnv());
